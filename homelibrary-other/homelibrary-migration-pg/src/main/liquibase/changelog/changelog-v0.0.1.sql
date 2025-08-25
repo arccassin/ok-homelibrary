31bb23fc23cc5caed82021262be9e3b0a7bf7f0a
arcassin <arccassin@gmail.com>
2025-08-25 15:37:45 +0300

    Fixed tests #4

--- homelibrary-other/homelibrary-migration-pg/src/main/liquibase/changelog/changelog-v0.0.1.sql
+++ homelibrary-other/homelibrary-migration-pg/src/main/liquibase/changelog/changelog-v0.0.1.sql
@@ -5,11 +5,11 @@
 CREATE TABLE "eds" (
 	"id" text primary key constraint eds_id_length_ctr check (length("id") < 64),
 	"title" text constraint eds_title_length_ctr check (length(title) < 256),
-	"author" text constraint eds_author_length_ctr check (length(title) < 256),
-	"isbn" text constraint eds_isbn_length_ctr check (length(title) < 128),
-	"year" text constraint eds_year_length_ctr check (length(title) < 6),
-	"owner_id" text not null constraint eds_owner_id_length_ctr check (length(id) < 64),
-	"lock" text not null constraint eds_lock_length_ctr check (length(id) < 64)
+	"author" text constraint eds_author_length_ctr check (length(author) < 256),
+	"isbn" text constraint eds_isbn_length_ctr check (length(isbn) < 128),
+	"year" text constraint eds_year_length_ctr check (length(year) < 6),
+	"owner_id" text not null constraint eds_owner_id_length_ctr check (length(owner_id) < 64),
+	"lock" text not null constraint eds_lock_length_ctr check (length(lock) < 64)
 );
 
 CREATE INDEX eds_owner_id_idx on "eds" using hash ("owner_id");
